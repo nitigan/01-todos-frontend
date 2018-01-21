385134a1ad7857ce3ed3466517fef33f926151bf
nitigan <nitigan.n@fitm.kmutnb.ac.th>
2018-01-21 15:02:37 +0700

    completed challenge 3

--- src/components/TodosFooter.vue
+++ src/components/TodosFooter.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
-    <p class="is-pulled-left">3 items left</p>
+    <p class="is-pulled-left">{{count}} items left</p>
     <visibility-input/>
   </div>
 </template>
 
 <script>
 import VisibilityInput from '@/components/VisibilityInput'
-
+import { mapGetters } from 'vuex'
 export default {
   components: {
     VisibilityInput
+  },
+  computed: {
+    ...mapGetters(['todos']),
+    count () {
+      return this.todos.filter(todo => todo.completed === false).length
+    }
   }
 }
 </script>
